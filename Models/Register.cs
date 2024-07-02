b87004cfe4dee72a96bfd64c7251b596becc2d24
Your Name <you@example.com>
2024-07-02 13:47:35 -0400

    comments-Gurwinder Singh

--- Models/Register.cs
+++ Models/Register.cs
@@ -2,22 +2,30 @@
 
 namespace FashionMart.Models
 {
+    // Done by Gurwinder Singh
     public class Register
     {
+        // The first name of the user.
         public string FirstName { get; set; }
+
+        // The last name of the user.
         public string LastName { get; set; }
 
+        // The password of the user is required
         [Required(ErrorMessage = "Password is required")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
+
+        // The password confirmation field
         [DataType(DataType.Password)]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
 
+        // The email of the user is required with email validation
         [Required(ErrorMessage = "Email is required")]
         [EmailAddress]
         public string Email { get; set; }
 
-      
     }
 }
+
