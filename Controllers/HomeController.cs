09c7f55909b120c45e8174a4b15cd8bff2567ed2
Bsingh1502 <bhanurana161122@gmail.com>
2024-07-02 12:43:47 -0400

    Sprint 2 push

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,112 @@
+﻿using FashionMart.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Security.Claims;
+
+namespace FashionMart.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+        public HomeController(ILogger<HomeController> logger, UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _logger = logger;
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(Register userModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(userModel);
+            }
+            var user = new User()
+            {
+                FirstName = userModel.FirstName,
+                LastName = userModel.LastName,
+                UserName = userModel.Email,
+                Email = userModel.Email,
+            };
+            var result = await _userManager.CreateAsync(user, userModel.Password);
+            if (!result.Succeeded)
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.TryAddModelError(error.Code, error.Description);
+                }
+                return View(userModel);
+            }
+            var res = await _userManager.AddToRoleAsync(user, "Visitor");
+            return View("Index");
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> LoginPost(Login userModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(userModel);
+            }
+            var result = await _signInManager.PasswordSignInAsync(userModel.Email, userModel.Password, userModel.RememberMe, false);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ModelState.AddModelError("", "Invalid UserName or Password");
+                return View("Login");
+            }
+        }
+
+
+        [Route("Account/Login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+
+        [Authorize]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index");
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ EntityFramework/DBContext.cs
@@ -0,0 +1,22 @@
+﻿using FashionMart.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace FashionMart.EntityFramework
+{
+    public class Context: IdentityDbContext<User>
+    {
+        public Context(DbContextOptions options) :base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+
+            modelBuilder.ApplyConfiguration(new UserRoles());
+        }
+
+    }
+}
